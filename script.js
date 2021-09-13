98b2e5dc19c7e66ebfd4f3b26baaf00d6fdf894c
rigole <foplacide@gmail.com>
2021-09-13 23:20:43 +0100

    setting the players + ball + middle

--- script.js
+++ script.js
@@ -56,29 +56,29 @@ function renderCanvas() {
     context.fillRect(0, 0, width, height)
 
     //Paddle Color
-    //context.fillStyle = 'white'
+    context.fillStyle = 'white'
 
     // Player Paddle (Bottom)
-    //context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
+    context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
 
     // Computer Paddle(Top)
-    //context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight)
+    context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight)
 
 
     // Dashed Center Line
-    //context.beginPath();
-    //context.setLineDash([4])
-    //context.moveTo(0, 350);
-    //context.lineTo(500, 350)
-    //context.strokeStyle = 'grey'
-    //context.stroke()
+    context.beginPath();
+    context.setLineDash([4])
+    context.moveTo(0, 350);
+    context.lineTo(500, 350)
+    context.strokeStyle = 'grey'
+    context.stroke()
 
 
     // Ball
-    //context.beginPath()
-    //context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false)
-    //context.fillStyle = 'white'
-    //context.fill()
+    context.beginPath()
+    context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false)
+    context.fillStyle = 'white'
+    context.fill()
 
     // Score
     //context.font = '32px Courier New'
